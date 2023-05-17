84f38f6338ef7f5283ec49df64159d6bbe800b4c
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-17 15:21:14 -0300

    Removendo classe Address

--- src/main/groovy/utils/baseperson/BasePerson.groovy
+++ src/main/groovy/utils/baseperson/BasePerson.groovy
@@ -1,15 +1,15 @@
 package utils.baseperson
 
-import utils.baseperson.Address
-
 class BasePerson {
 
     String name
     String email
     String cpfCnpj
     String phone
-    Address address
-
-    static embedded = ['address']
+    String state
+    String city
+    String district
+    String number
+    String postalCode
 
 }
