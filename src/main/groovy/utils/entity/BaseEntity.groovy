77c5765491f9a7a8dee863943e84e794c2e4e496
Vinicius Wille <vinicius.wille@asaas.com.br>
2023-05-17 17:58:17 -0300

    Tipando os campos corretamente - adicionando tablePerHierarchy

--- src/main/groovy/utils/entity/BaseEntity.groovy
+++ src/main/groovy/utils/entity/BaseEntity.groovy
@@ -1,14 +1,13 @@
 package utils.entity
 
-import java.time.LocalDateTime
-
 abstract class BaseEntity {
 
-    long id
-    LocalDateTime createdAt
-    boolean deleted
+    Date createdAt
+    Date lastUpdated
+    Boolean deleted
 
     static mapping = {
         version false
+        tablePerHierarchy false
     }
 }
