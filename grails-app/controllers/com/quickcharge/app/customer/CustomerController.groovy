9d3bd27c0dd8ac2ba45f249282dfc1782cacade7
Vinicius Wille <vinicius.wille@asaas.com.br>
2023-05-18 16:23:49 -0300

    Criando controller do customer

---
+++ grails-app/controllers/com/quickcharge/app/customer/CustomerController.groovy
@@ -0,0 +1,23 @@
+package com.quickcharge.app.customer
+
+class CustomerController {
+
+    def customerService
+
+    def create() {
+        return [:]
+    }
+
+    def save() {
+        Customer customer = customerService.save(params)
+        if (customer.hasErrors()) {
+            redirect([
+                action: "create"
+                ])
+            return
+        }
+
+        redirect(action: "create")
+        return
+    }
+}
