a189ce6f190585049626d1d5b22350c9a2be16df
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-19 11:55:18 -0300

    Lint modificando classe

--- grails-app/domain/com/quickcharge/app/payer/Payer.groovy
+++ grails-app/domain/com/quickcharge/app/payer/Payer.groovy
@@ -2,6 +2,8 @@ package com.quickcharge.app.payer
 
 import utils.baseperson.BasePerson
 
-class Payer extends BasePerson{
-    static constraints = {}
+class Payer extends BasePerson {
+
+    static constraints = { }
+
 }
