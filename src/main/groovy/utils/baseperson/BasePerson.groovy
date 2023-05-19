cab39df488bcfc9117db2d2f3b96bead2507241e
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-19 15:43:41 -0300

    Adicionando constraints de BasePerson

--- src/main/groovy/utils/baseperson/BasePerson.groovy
+++ src/main/groovy/utils/baseperson/BasePerson.groovy
@@ -12,4 +12,16 @@ abstract class BasePerson {
     String number
     String postalCode
 
+    static constraints = {
+        name blank: false, nullable: false
+        email blank: false, nullable: false, email: true
+        cpfCnpj blank: false, nullable: false, size: 11..14
+        phone blank: false, nullable: false, size: 10..11
+        state blank: false, nullable: false, size: 2
+        city blank: false, nullable: false
+        district blank: false, nullable: false
+        number blank: false, nullable: false
+        postalCode blank: false, nullable: false, size: 8
+    }
+
 }
