e13ada439f05a23b5b5fa6b2b0c7d1b9cff60aa8
Jezreel Moraes <130087441+Jezreel-Moraes-Asaas@users.noreply.github.com>
2023-05-22 11:15:33 -0300

    Update grails-app/domain/com/quickcharge/app/payer/Payer.groovy
    Co-authored-by: welington.loh <welington.loh@gmail.com>

--- grails-app/domain/com/quickcharge/app/payer/Payer.groovy
+++ grails-app/domain/com/quickcharge/app/payer/Payer.groovy
@@ -3,7 +3,7 @@ package com.quickcharge.app.payer
 import com.quickcharge.app.customer.Customer
 import utils.baseperson.BasePerson
 
-class Payer extends BasePerson{
+class Payer extends BasePerson {
     Customer customer
     static constraints = {}
 }
