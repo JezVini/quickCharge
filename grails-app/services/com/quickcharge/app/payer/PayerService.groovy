e7f38ef70a1ab46cb692090f7166c79139859d9e
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-23 15:26:12 -0300

    Modificando formatação

--- grails-app/services/com/quickcharge/app/payer/PayerService.groovy
+++ grails-app/services/com/quickcharge/app/payer/PayerService.groovy
@@ -10,54 +10,58 @@ class PayerService {
     public Payer save(Map params) {
         Payer payer = validatePayer(params)
 
-        if (payer.hasErrors()) {
-            throw new ValidationException(null, payer.errors)
-        }
-
         payer.properties["name", "email", "cpfCnpj", "phone", "state", "city", "district", "number", "postalCode"] = params
         payer.customer = Customer.get(params.customer)
 
-        payer.save(failOnError: true)
-
         if (payer.hasErrors()) {
-            throw new ValidationException(null, payer.errors)
+            throw new ValidationException("Erro ao salvar pagador", payer.errors)
         }
+
+        payer.save(failOnError: true)
 
         return payer
     }
 
     public Payer validatePayer(Map params) {
-        Payer payer = new Payer()
+        Payer validatedPayer = new Payer()
 
         if (!params.name) {
             payer.errors.reject("", null, "O campo nome é obrigatório")
         }
+
         if (!params.email) {
             payer.errors.reject("", null, "O campo e-mail é obrigatório")
         }
+
         if (!params.cpfCnpj) {
             payer.errors.reject("", null, "O campo CPF ou CNPJ é obrigatório")
         }
+
         if (!params.phone) {
             payer.errors.reject("", null, "O campo telefone é obrigatório")
         }
+
         if (!params.state) {
             payer.errors.reject("", null, "O campo estado é obrigatório")
         }
+
         if (!params.city) {
             payer.errors.reject("", null, "O campo cidade é obrigatório")
         }
+
         if (!params.district) {
             payer.errors.reject("", null, "O campo bairro é obrigatório")
         }
+
         if (!params.number) {
             payer.errors.reject("", null, "O campo número é obrigatório")
         }
+
         if (!params.postalCode) {
             payer.errors.reject("", null, "O campo CEP é obrigatório")
         }
 
-        return payer
+        return validatedPayer
     }
 
 }
