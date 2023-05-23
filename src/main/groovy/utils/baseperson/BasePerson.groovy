8e3f0038f6e062599f8b8c515f741cbd651882f5
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-23 15:32:28 -0300

    Removendo constraints desnecessarias

--- src/main/groovy/utils/baseperson/BasePerson.groovy
+++ src/main/groovy/utils/baseperson/BasePerson.groovy
@@ -1,5 +1,8 @@
 package utils.baseperson
 
+
+
+@DirtyCheck
 abstract class BasePerson {
 
     String name
@@ -13,15 +16,15 @@ abstract class BasePerson {
     String postalCode
 
     static constraints = {
-        name blank: false, nullable: false
-        email blank: false, nullable: false, email: true
-        cpfCnpj blank: false, nullable: false, size: 11..14
-        phone blank: false, nullable: false, size: 10..11
-        state blank: false, nullable: false, size: 2
-        city blank: false, nullable: false
-        district blank: false, nullable: false
-        number blank: false, nullable: false
-        postalCode blank: false, nullable: false, size: 8
+        name blank: false
+        email blank: false, email: true
+        cpfCnpj blank: false, size: 11..14
+        phone blank: false, size: 10..11
+        state blank: false, size: 2
+        city blank: false
+        district blank: false
+        number blank: false
+        postalCode blank: false, size: 8
     }
 
 }
