fb3bd7d228215ab7f01fc24590970192f8a9798d
Vinicius Wille <vinicius.wille@asaas.com.br>
2023-05-24 15:04:16 -0300

    Implementando o namedqueries em customer e payer

--- grails-app/domain/com/quickcharge/app/payer/Payer.groovy
+++ grails-app/domain/com/quickcharge/app/payer/Payer.groovy
@@ -4,7 +4,16 @@ import com.quickcharge.app.customer.Customer
 import utils.baseperson.BasePerson
 
 class Payer extends BasePerson {
+    
     Customer customer
 
-    static constraints = {}
+    static namedQueries = {
+        query { Map search ->
+            if (Boolean.valueOf(search.deletedOnly)) {
+                eq("deleted", true)
+            } else if (!Boolean.valueOf(search.includeDeleted)) {
+                eq("deleted", false)
+            }
+        }
+    }
 }
