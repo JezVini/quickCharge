74f51363024d867925575a44e489a97c84975ee2
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-24 10:58:12 -0300

    Adicionando update

--- grails-app/controllers/com/quickcharge/app/payer/PayerController.groovy
+++ grails-app/controllers/com/quickcharge/app/payer/PayerController.groovy
@@ -26,10 +26,31 @@ class PayerController {
             flash.message = validationException.errors.allErrors.first().defaultMessage
         } catch (Exception exception) {
             flash.message = "Ocorreu um erro ao criar pagador, contate o desenvolvimento"
+            log.info("PayerController.save >> Erro ao salvar pagador: ${params}")
         } finally {
             redirect([
                 action: "create",
                 params: params
+            ])
+        }
+    }
+
+    def update() {
+        try {
+            payerService.saveOrUpdate(params)
+            flash.message = "Pagador editado com sucesso"
+        } catch (ValidationException validationException) {
+            flash.message = validationException.errors.allErrors.first().defaultMessage
+        } catch (Exception exception) {
+            flash.message = "Ocorreu um erro ao editar pagador, contate o desenvolvimento"
+            log.info("PayerController.save >> Erro ao editar pagador: ${params}")
+        } finally {
+            redirect([
+                action: "edit",
+                params: [
+                    id: params.id,
+                    customerId: params.customerId
+                ]
             ])
         }
     }
