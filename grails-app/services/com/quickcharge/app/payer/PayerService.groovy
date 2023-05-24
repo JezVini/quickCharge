27c08d7fe30ff6efc16fd85f0d01da10dd2b50af
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-24 09:33:39 -0300

    Removendo o retorno de params para create e alterando nomes

--- grails-app/services/com/quickcharge/app/payer/PayerService.groovy
+++ grails-app/services/com/quickcharge/app/payer/PayerService.groovy
@@ -8,17 +8,26 @@ import com.quickcharge.app.customer.Customer
 class PayerService {
 
     public Payer save(Map params) {
-        Payer payer = validatePayer(params)
-
-        payer.properties["name", "email", "cpfCnpj", "phone", "state", "city", "district", "number", "postalCode"] = params
-        payer.customer = Customer.get(params.customer)
+        Payer payer = validateSave(params)
 
         if (payer.hasErrors()) {
             throw new ValidationException("Erro ao salvar pagador", payer.errors)
         }
 
-        payer.save(failOnError: true)
+        payer.customer = Customer.get(params.customerId)
+        payer.properties[
+            "name",
+            "email",
+            "cpfCnpj",
+            "phone",
+            "state",
+            "city",
+            "district",
+            "number",
+            "postalCode"
+        ] = params
 
+        payer.save(failOnError: true)
         return payer
     }
 
@@ -26,42 +35,41 @@ class PayerService {
         Payer validatedPayer = new Payer()
 
         if (!params.name) {
-            payer.errors.reject("", null, "O campo nome é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo nome é obrigatório")
         }
 
         if (!params.email) {
-            payer.errors.reject("", null, "O campo e-mail é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo e-mail é obrigatório")
         }
 
         if (!params.cpfCnpj) {
-            payer.errors.reject("", null, "O campo CPF ou CNPJ é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo CPF ou CNPJ é obrigatório")
         }
 
         if (!params.phone) {
-            payer.errors.reject("", null, "O campo telefone é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo telefone é obrigatório")
         }
 
         if (!params.state) {
-            payer.errors.reject("", null, "O campo estado é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo estado é obrigatório")
         }
 
         if (!params.city) {
-            payer.errors.reject("", null, "O campo cidade é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo cidade é obrigatório")
         }
 
         if (!params.district) {
-            payer.errors.reject("", null, "O campo bairro é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo bairro é obrigatório")
         }
 
         if (!params.number) {
-            payer.errors.reject("", null, "O campo número é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo número é obrigatório")
         }
 
         if (!params.postalCode) {
-            payer.errors.reject("", null, "O campo CEP é obrigatório")
+            validatedPayer.errors.reject("", null, "O campo CEP é obrigatório")
         }
 
         return validatedPayer
     }
-
 }
