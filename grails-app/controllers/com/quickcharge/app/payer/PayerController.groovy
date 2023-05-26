80276543de028eaf5557a1363dd448961dec7709
Vinicius Wille <vinicius.wille@asaas.com.br>
2023-05-26 09:38:53 -0300

    Movendo MessageType e aplicando nos controllers

--- grails-app/controllers/com/quickcharge/app/payer/PayerController.groovy
+++ grails-app/controllers/com/quickcharge/app/payer/PayerController.groovy
@@ -1,6 +1,7 @@
 package com.quickcharge.app.payer
 
 import grails.validation.ValidationException
+import utils.message.MessageType
 
 class PayerController {
 
@@ -14,10 +15,13 @@ class PayerController {
         try {
             payerService.save(params)
             flash.message = "Pagador criado com sucesso"
+            flash.type = MessageType.SUCCESS
         } catch (ValidationException validationException) {
             flash.message = validationException.errors.allErrors.first().defaultMessage
+            flash.type = MessageType.WARNING
         } catch (Exception exception) {
             flash.message = "Ocorreu um erro ao criar pagador, contate o desenvolvimento"
+            flash.type = MessageType.ERROR
             log.info("PayerController.save >> Erro ao salvar pagador: ${params}")
         } finally {
             redirect([
