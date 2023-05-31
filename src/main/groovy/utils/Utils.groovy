878440515068ce747478f8cc67bc4f8d04472e8f
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-31 12:00:55 -0300

    Adicionando validação de pattern matching de telefone em Utils

--- src/main/groovy/utils/Utils.groovy
+++ src/main/groovy/utils/Utils.groovy
@@ -1,10 +1,19 @@
 package utils
 
-class Utils {
+import java.util.regex.Pattern
 
+class Utils {
+    
+    public static final Pattern PHONE_PATTERN = ~/\(\d{2}\) 9?\d{4}-?\d{4}/
+    
     public static String removeNonNumeric(String text) {
         if (text == null) return null
 
         return text?.replaceAll("\\D+","")
     }
+
+    public static Boolean isPhonePatternMatch(String phone) {
+        return phone.matches(PHONE_PATTERN)
+    }
+    
 }
