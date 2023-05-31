65c9fdfb28f8e86a9522d8d1cb6cd6652893715e
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-31 10:28:24 -0300

    Adicionando os campos de payment

--- grails-app/domain/com/quickcharge/app/payment/Payment.groovy
+++ grails-app/domain/com/quickcharge/app/payment/Payment.groovy
@@ -1,8 +1,18 @@
 package com.quickcharge.app.payment
 
+import com.quickcharge.app.customer.Customer
+import com.quickcharge.app.payer.Payer
 import utils.entity.BaseEntity
 
-class Payment extends BaseEntity{
+class Payment extends BaseEntity {
+
+    Payer payer
+    Customer customer
+    String billingType
+    Double value
+    String status
+    Date dueDate
+    Date paymentDate
     
     static constraints = {
     }
