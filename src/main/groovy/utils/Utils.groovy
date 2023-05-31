4a6254d765f17560dc8d064223410a14a01f1851
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-05-31 12:03:16 -0300

    Adicionando validação de pattern matching de CEP em Utils

--- src/main/groovy/utils/Utils.groovy
+++ src/main/groovy/utils/Utils.groovy
@@ -5,6 +5,7 @@ import java.util.regex.Pattern
 class Utils {
     
     public static final Pattern PHONE_PATTERN = ~/\(\d{2}\) 9?\d{4}-?\d{4}/
+    public static final Pattern POSTAL_CODE_PATTERN = ~/\d{5}-\d{3}/
     
     public static String removeNonNumeric(String text) {
         if (text == null) return null
@@ -14,6 +15,10 @@ class Utils {
 
     public static Boolean isPhonePatternMatch(String phone) {
         return phone.matches(PHONE_PATTERN)
+    }
+    
+    public static Boolean isPostalCodePatternMatch(String postalCode) {
+        return postalCode.matches(POSTAL_CODE_PATTERN)
     }
     
 }
