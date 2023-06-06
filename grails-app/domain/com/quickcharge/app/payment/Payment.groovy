e453811660059bd8459f1474746dc917740513bc
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-06-06 14:24:10 -0300

    Criando named queries de Payment

--- grails-app/domain/com/quickcharge/app/payment/Payment.groovy
+++ grails-app/domain/com/quickcharge/app/payment/Payment.groovy
@@ -27,4 +27,28 @@ class Payment extends BaseEntity {
             }
         }
     }
+
+    static namedQueries = {
+        query { Map search ->
+            if (!search.containsKey("customerId")) {
+                throw new RuntimeException("Payer.query(): o atributo [customerId] é obrigatório para executar a consulta.")
+            }
+
+            if (Boolean.valueOf(search.deletedOnly)) {
+                eq("deleted", true)
+            } else if (!Boolean.valueOf(search.includeDeleted)) {
+                eq("deleted", false)
+            }
+            
+            eq("customer.id", Long.valueOf(search.customerId))
+            
+            if (search.containsKey("payerId")) {
+                eq("payer.id", Long.valueOf(search.payerId))
+            }
+
+            if (search.containsKey("id")) {
+                eq("id", Long.valueOf(search.id))
+            }
+        }
+    }
 }
