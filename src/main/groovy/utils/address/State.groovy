32e5c95c6a733aa37719b803d5b69c8944f37c5d
Vinicius Wille <vinicius.wille@asaas.com.br>
2023-06-06 16:24:31 -0300

    Criação do enum de estados

---
+++ src/main/groovy/utils/address/State.groovy
@@ -0,0 +1,56 @@
+package utils.address
+
+enum State {
+    AC("Acre"),
+    AL("Alagoas"),
+    AP("Amapá"),
+    AM("Amazonas"),
+    BA("Bahia"),
+    CE("Ceará"),
+    DF("Distrito Federal"),
+    ES("Espírito Santo"),
+    GO("Goiás"),
+    MA("Maranhão"),
+    MT("Mato Grosso"),
+    MS("Mato Grosso do Sul"),
+    MG("Minas Gerais"),
+    PA("Pará"),
+    PB("Paraíba"),
+    PR("Paraná"),
+    PE("Pernambuco"),
+    PI("Piauí"),
+    RR("Roraima"),
+    RO("Rondônia"),
+    RJ("Rio de Janeiro"),
+    RN("Rio Grande do Norte"),
+    RS("Rio Grande do Sul"),
+    SC("Santa Catarina"),
+    SP("São Paulo"),
+    SE("Sergipe"),
+    TO("Tocantins")
+
+    private final String description
+
+    State(String description) {
+        this.description = description
+    }
+
+    public String getDescription() {
+        return this.description
+    }
+
+    static Boolean validate(value) {
+        if (!value) return true
+
+        try {
+            value as State
+            return true
+        } catch (IllegalArgumentException e) {
+            return false
+        }
+    }
+
+    static State findByDescription(String description) {
+        return State.values().find {state -> state.description == description}
+    }
+}
