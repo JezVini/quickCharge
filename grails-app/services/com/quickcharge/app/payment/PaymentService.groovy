6c25c693d73ab4158a3e5f8fcba9be4166ef6e10
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-06-09 22:35:35 -0300

    Unificando validação de payment delete e restore

--- grails-app/services/com/quickcharge/app/payment/PaymentService.groovy
+++ grails-app/services/com/quickcharge/app/payment/PaymentService.groovy
@@ -6,7 +6,7 @@ import grails.gorm.transactions.Transactional
 import grails.plugin.springsecurity.SpringSecurityService
 import grails.validation.ValidationException
 import org.apache.commons.lang3.EnumUtils
-import utils.payment.BillingType    
+import utils.payment.BillingType
 
 import java.text.SimpleDateFormat
 
@@ -64,51 +64,38 @@ class PaymentService {
     }
     
     public Payment delete(Map parameterMap) {
-        Payment validatedPayment = validateDelete(parameterMap)
+        Long customerId = Long.valueOf(springSecurityService.getCurrentUser().customer.id)
+        Map parameterQuery = [id: parameterMap.id, customerId: customerId]
+        Payment validatedPayment = validatePayment(parameterQuery)
 
         if (validatedPayment.hasErrors()) {
             throw new ValidationException("Erro ao remover cobrança", validatedPayment.errors)
         }
 
-        Long customerId = Long.valueOf(springSecurityService.getCurrentUser().customer.id)
-        Payment payment = Payment.query([id: parameterMap.id, customerId: customerId]).get()
+        Payment payment = Payment.query(parameterQuery).get()
         payment.deleted = true
 
         return payment.save(failOnError: true)
     }
- 
-    private Payment validateDelete(Map parameterMap) {
-        Payment validatedPayment = new Payment()
-
-        Long customerId = Long.valueOf(springSecurityService.getCurrentUser().customer.id)
-        Payment payment = Payment.query([id: parameterMap.id, customerId: customerId]).get()
-        if (!payment) {
-            validatedPayment.errors.rejectValue("id", "not.found")
-        }
-        
-        return validatedPayment
-    }
 
     public Payment restore(Map parameterMap) {
-        Payment validatedPayment = validateRestore(parameterMap)
+        Long customerId = Long.valueOf(springSecurityService.getCurrentUser().customer.id)
+        Map parameterQuery = [id: parameterMap.id, customerId: customerId, deletedOnly: true]
+        Payment validatedPayment = validatePayment(parameterQuery)
 
         if (validatedPayment.hasErrors()) {
-            throw new ValidationException("Erro ao restaurar cobrança", validatedPayment.errors)
+            throw new ValidationException("Erro ao remover cobrança", validatedPayment.errors)
         }
-
-        Long customerId = Long.valueOf(springSecurityService.getCurrentUser().customer.id)
-        Payment payment = Payment.query([id: parameterMap.id, customerId: customerId, deletedOnly: true]).get()
+        
+        Payment payment = Payment.query(parameterQuery).get()
         payment.deleted = false
 
         return payment.save(failOnError: true)
     }
-
-    private Payment validateRestore(Map parameterMap) {
+    
+    private Payment validatePayment(Map parameterQuery) {
         Payment validatedPayment = new Payment()
-
-        Long customerId = Long.valueOf(springSecurityService.getCurrentUser().customer.id)
-        Payment payment = Payment.query([id: parameterMap.id, customerId: customerId, deletedOnly: true]).get()
-        if (!payment) {
+        if (!Payment.query(parameterQuery).get()) {
             validatedPayment.errors.rejectValue("id", "not.found")
         }
 
