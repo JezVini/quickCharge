3eb2a8dc4d309569680f3dd7b3bd6f54b07e3520
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-06-15 11:37:19 -0300

    fix: Implementando external-config para variáveis de ambiente

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -28,15 +28,6 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 
 grails.assets.excludes = ["mixins/*.scss"]
 
-grails {
-    mail {
-        host = "smtp.gmail.com"
-        port = 465
-        username = ""
-        password = ""
-        props = ["mail.smtp.auth"                  : "true",
-                 "mail.smtp.socketFactory.port"    : "465",
-                 "mail.smtp.socketFactory.class"   : "javax.net.ssl.SSLSocketFactory",
-                 "mail.smtp.socketFactory.fallback": "false"]
-    }
-}
+grails.config.locations = [
+    "file:///quickcharge/application.groovy",
+]
