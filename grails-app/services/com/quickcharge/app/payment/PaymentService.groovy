b9a3592e2f908d572dc5904a718ffe9d504fc1af
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2023-06-26 11:47:43 -0300

    feat: Implementando paginatedPayment em PaymentService

--- grails-app/services/com/quickcharge/app/payment/PaymentService.groovy
+++ grails-app/services/com/quickcharge/app/payment/PaymentService.groovy
@@ -2,13 +2,13 @@ package com.quickcharge.app.payment
 
 import com.quickcharge.app.customer.Customer
 import com.quickcharge.app.payer.Payer
+import grails.gorm.PagedResultList
 import grails.gorm.transactions.Transactional
 import grails.validation.ValidationException
 import org.apache.commons.lang3.EnumUtils
 import utils.Utils
 import utils.payment.BillingType
 import utils.payment.PaymentStatus
-import org.apache.commons.lang3.time.DateUtils
 
 import java.text.SimpleDateFormat
 
@@ -19,58 +19,58 @@ class PaymentService {
 
     def save(Map parameterMap, Customer customer) {
         Payment validatedPayment = validateSave(parameterMap)
-        
+
         if (validatedPayment.hasErrors()) {
             throw new ValidationException("Erro ao salvar cobrança", validatedPayment.errors)
         }
-        
+
         Payment payment = new Payment()
         Payer payer = Payer.query([id: parameterMap.payerId, customerId: customer.id]).get()
         BillingType billingType = BillingType[parameterMap.billingType as String] as BillingType
         Double value = Utils.toBigDecimalFormatted(parameterMap.value as String).toDouble()
-        Date dueDate = new SimpleDateFormat("dd/MM/yyyy").parse(parameterMap.dueDate as String) 
-        
+        Date dueDate = new SimpleDateFormat("dd/MM/yyyy").parse(parameterMap.dueDate as String)
+
         payment.payer = payer
         payment.customer = customer
         payment.billingType = billingType
         payment.value = value
         payment.dueDate = dueDate
-        
-        payment.save(failOnError: true)        
+
+        payment.save(failOnError: true)
     }
-    
+
     public Payment validateSave(Map parameterMap) {
         Payment validatedPayment = new Payment()
 
-        if(!parameterMap.payerId) {
+        if (!parameterMap.payerId) {
             validatedPayment.errors.rejectValue("payer", "not.selected")
         }
-        
-        if(!EnumUtils.isValidEnum(BillingType.class, parameterMap.billingType as String)) {
+
+        if (!EnumUtils.isValidEnum(BillingType.class, parameterMap.billingType as String)) {
             validatedPayment.errors.rejectValue("billingType", "not.selected")
         }
-        
-        if(!parameterMap.dueDate) {
+
+        if (!parameterMap.dueDate) {
             validatedPayment.errors.rejectValue("dueDate", "not.selected",)
             return validatedPayment
         }
 
         SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy")
         Date dueDate = simpleDate.parse(parameterMap.dueDate)
-        if(dueDate.before(simpleDate.parse(simpleDate.format(new Date())))) {
+        if (dueDate.before(simpleDate.parse(simpleDate.format(new Date())))) {
             validatedPayment.errors.rejectValue("dueDate", "past.date")
         }
-        
+
         return validatedPayment
     }
-    
+
     public Payment delete(Map parameterMap, Customer customer) {
         Payment payment = Payment.getById(parameterMap.id, customer.id)
         if (!payment.status.canUpdate()) {
             payment.errors.rejectValue("status", "already.received")
             throw new ValidationException("Erro ao remover cobrança", payment.errors)
         }
-        
+
         payment.deleted = true
 
         return payment.save(failOnError: true)
@@ -87,7 +87,7 @@ class PaymentService {
 
         return payment.save(failOnError: true)
     }
-    
+
     public Payment receiveInCash(Map parameterMap, Customer customer) {
         Payment payment = Payment.getById(parameterMap.id, customer.id)
         if (!payment.status.canUpdate()) {
@@ -97,7 +97,7 @@ class PaymentService {
 
         payment.status = PaymentStatus.RECEIVED_IN_CASH
         payment.paymentDate = new Date()
-        
+
         paymentReceiptService.createReceipt(payment)
         return payment.save(failOnError: true)
     }
@@ -126,7 +126,7 @@ class PaymentService {
 
         SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy")
         Date dueDateValidate = simpleDate.parse(dueDate)
-        if(dueDateValidate.before(simpleDate.parse(simpleDate.format(new Date())))) {
+        if (dueDateValidate.before(simpleDate.parse(simpleDate.format(new Date())))) {
             validatedPayment.errors.rejectValue("dueDate", "past.date")
         }
 
@@ -151,5 +151,20 @@ class PaymentService {
                 }
             }
         }
+    }
+
+    public PagedResultList<Payment> paginatedPayment(Map parameterMap, Customer customer) {
+        PagedResultList<Payment> pagedResultList = Payment.query([
+            customerId    : customer.id,
+            deletedOnly   : parameterMap.deletedOnly,
+            includeDeleted: parameterMap.includeDeleted
+        ]).list([
+            max   : parameterMap.max,
+            offset: parameterMap.offset,
+            sort  : "id",
+            order : "desc"
+        ])
+
+        return pagedResultList
     }
 }
